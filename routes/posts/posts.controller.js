d94f06738ff4cf4569e8c517f9cf85d27945fca9
doowankim <rlaendhks11@gmail.com>
2021-01-07 22:10:36 +0900

    Added PUT method API

--- routes/posts/posts.controller.js
+++ routes/posts/posts.controller.js
@@ -61,8 +61,50 @@ exports.create = async ctx => {
 };
 
 // Post 수정 API
-exports.replace = ctx => {
-	ctx.body = 'replaced post';
+exports.replace = async ctx => {
+	const { id } = ctx.params;
+
+	if (!ObjectId.isValid(id)) {
+		ctx.status = 400; // Bad Request
+		return;
+	}
+
+	// 검증할 스키마
+	const schema = Joi.object().keys({
+		title: Joi.string().required(),
+		author: Joi.string().required(),
+		body: Joi.string().required(),
+		tags: Joi.array().items(
+			Joi.object().keys({
+				skill: Joi.string().required(),
+			}),
+		),
+		attachedFile: Joi.string().required(),
+	});
+
+	// validate를 통하여 검증
+	const validation = schema.validate(ctx.request.body);
+
+	// 스키마가 잘못됐을 경우
+	if (validation.error) {
+		ctx.status = 400; // Bad Request
+		ctx.body = validation.error;
+		return;
+	}
+
+	let post;
+
+	try {
+		post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+			upsert: true, // 데이터가 존재하지 않으면 새로 만들어준다.
+			new: true, // 반환하는 값이 업데이트된 데이터
+			// new: true가 없으면 ctx.body = post 했을 때 업데이트 전의 데이터를 보여줌
+		});
+	} catch (e) {
+		return ctx.throw(500, e);
+	}
+
+	ctx.body = post;
 };
 
 // Post 부분 수정 API
