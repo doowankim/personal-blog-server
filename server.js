a9964bb07d5869739cbddd108c748c6baa95391b
doowankim <rlaendhks11@gmail.com>
2021-01-07 18:02:13 +0900

    Added mongoose & env

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
@@ -5,9 +7,19 @@ const app = new Koa();
 const router = new Router();
 const postRoute = require('./routes/posts');
 
+const mongoose = require('mongoose');
+
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('MongoDB Connected...'))
+	.catch(e => console.errer(e));
+
 router.use('/posts', postRoute.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-const port = 3400;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is listening to port ${port}`));
